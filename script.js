9a5040be7f14f335ecbf60188f7481ea17bcd381
Manuel Zacarias <159145623+Manuelzacarias201@users.noreply.github.com>
2024-09-16 22:52:30 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const trafficButton = document.querySelector('.hero .button');
+    
+    trafficButton.addEventListener('click', function() {
+        alert('Mostrando el tráfico actual en Tuxtla Gutiérrez...');
+        // Aquí se puede incluir para que se muestre el trafico
+    });
+});
